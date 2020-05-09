3bd1b1c0c6bb682d77faf09f02d45bac0cf52370
DBarinovv <9653314755@bk.ru>
2020-05-09 23:42:14 +0300

    With my_elf.h and defaut headers

---
+++ Bin_translator.cpp
@@ -0,0 +1,129 @@
+
+//{============================================================================
+//{                             BINARY TRANSLATOR                             ;
+//}============================================================================
+
+#include <stdio.h>
+#include "my_elf.h"
+
+//=============================================================================
+
+const elf::Elf64_Half	C_my_e_type      = 0x0002;
+const elf::Elf64_Half	C_my_e_machine   = 0x003E;
+const elf::Elf64_Word	C_my_e_version   = 0x00000001;
+const elf::Elf64_Addr	C_my_e_entry     = 0x0000000000401000; // tested asm programs
+const elf::Elf64_Off	C_my_e_phoff     = 0x0000000000000040;
+const elf::Elf64_Off	C_my_e_shoff     =; // EOF because without any sections
+const elf::Elf64_Word	C_my_e_flags     = 0x00000000;
+const elf::Elf64_Half	C_my_e_ehsize    = 0x0040; // 64 bit
+const elf::Elf64_Half	C_my_e_phentsize = 0x0038; // 64 bit
+const elf::Elf64_Half	C_my_e_phnum;    = 0x0003; // ??? (default, .data and .bss, .text)
+const elf::Elf64_Half	C_my_e_shentsize = 0x0040; // 64 bit
+const elf::Elf64_Half	C_my_e_shnum     = 0x0000; // because without any sections
+const elf::Elf64_Half	C_my_e_shstrndx  = 0x0000; // because without any sections
+
+
+const elf::Elf64_Word	C_my_p_type   = 0x00000001;
+const elf::Elf64_Word	C_my_p_flags  = 0x00000004;
+const elf::Elf64_Off 	C_my_p_offset = 0x0000000000000000;
+const elf::Elf64_Addr	C_my_p_vaddr  = 0x0000000000400000;
+const elf::Elf64_Addr	C_my_p_paddr  = 0x0000000000400000;
+const elf::Elf64_Xword	C_my_p_filesz = 0x00000000000000B0;
+const elf::Elf64_Xword	C_my_p_memsz  = 0x00000000000000B0;
+const elf::Elf64_Xword	C_my_p_align  = 0x0000000000001000;
+
+//=============================================================================
+
+void Make_Default_ELF_Header (elf::Elf64_Ehdr *elf_header);
+
+void Make_Default_Program_Header (elf::Elf64_Phdr *program_header);
+
+//=============================================================================
+
+int main ()
+{
+    FILE *fin  = fopen ("program.txt",  "rb");
+    FILE *fout = fopen ("Elf_file", "wb");
+
+    elf::Elf64_Ehdr elf_header = {};
+    Make_Default_ELF_Header (&elf_header);
+
+    return 0;
+}
+
+//=============================================================================
+
+void Make_Default_ELF_Header (elf::Elf64_Ehdr *elf_header)
+{
+    elf_header->e_ident[0] = '';  // 0x7f
+    elf_header->e_ident[1] = 'E';
+    elf_header->e_ident[2] = 'L';
+    elf_header->e_ident[3] = 'F';
+
+    elf_header->e_ident[4] = 0x02;
+    elf_header->e_ident[5] = 0x01;
+    elf_header->e_ident[6] = 0x01;
+    elf_header->e_ident[7] = 0x00;
+    elf_header->e_ident[8] = 0x00;
+
+    elf_header->e_type      = C_my_e_type;
+    elf_header->e_machine   = C_my_e_machine;
+    elf_header->e_version   = C_my_e_version;
+    elf_header->e_entry     = C_my_e_entry;
+    elf_header->e_phoff     = C_my_e_phoff;
+    elf_header->e_shoff     = C_my_e_shoff;
+    elf_header->e_flags     = C_my_e_flags;
+    elf_header->e_ehsize    = C_my_e_ehsize;
+    elf_header->e_phentsize = C_my_e_phentsize;
+    elf_header->e_phnum     = C_my_e_phnum;
+    elf_header->e_shentsize = C_my_e_shentsize;
+    elf_header->e_shnum     = C_my_e_shnum;
+    elf_header->e_shstrndx  = C_my_e_shstrndx;
+}
+
+//-----------------------------------------------------------------------------
+
+void Make_Default_Program_Header (elf::Elf64_Phdr *program_header)
+{
+    program_header->p_type	 = C_my_p_type;
+    program_header->p_flags	 = C_my_p_flags;
+    program_header->p_offset = C_my_p_offset;
+    program_header->p_vaddr	 = C_my_p_vaddr;
+    program_header->p_paddr	 = C_my_p_paddr;
+    program_header->p_filesz = C_my_p_filesz;
+    program_header->p_memsz	 = C_my_p_memsz;
+    program_header->p_align  = C_my_p_align;
+}
+
+
+//Name           Offset         NumValue            Value
+//EI_MAG:        0x00000000     0x7F454C46          ELF
+//EI_CLASS       0x00000004     0x02                64 BIT
+//EI_DATA        0x00000005     0x01                DATA2LSB (Little-Endian)
+//EI_VERSION     0x00000006     0x01                EV_CURRENT
+//EI_OSABI       0x00000007     0x00                UNIX System V ABI
+//EI_OSABIVER    0x00000008     0x00
+//E_TYPE         0x00000010     0x0002              ET_EXEC (Executable file)
+//E_MACHINE      0x00000012     0x003E              EM_X86_64 (AMD x86- 64 architecture)
+//E_VERSION      0x00000014     0x00000001          EV_CURRENT
+//E_ENTRY        0x00000018     0x0000000000401000
+//E_PHOFF        0x00000020     0x0000000000000040
+//E_SHOFF        0x00000028     0x0000000000001120
+//E_FLAGS        0x00000030     0x00000000
+//E_EHSIZE       0x00000034     0x0040
+//E_PHENTSIZE    0x00000036     0x0038
+//E_PHNUM        0x00000038     0x0002
+//E_SHENTSIZE    0x0000003A     0x0040
+//E_SHNUM        0x0000003C     0x0005
+//E_SHSTRNDX     0x0000003E     0x0004
+
+//-----------------------------------------------------------------------------
+
+//P_TYPE         0x00000040     0x00000001          PT_LOAD (Loadable segment)
+//P_FLAGS        0x00000044     0x00000004          Read
+//P_OFFSET       0x00000048     0x0000000000000000
+//P_VADDR        0x00000050     0x0000000000400000
+//P_PADDR        0x00000058     0x0000000000400000
+//P_FILESZ       0x00000060     0x00000000000000B0
+//P_MEMSZ        0x00000068     0x00000000000000B0
+//P_ALIGN        0x00000070     0x0000000000001000
---
+++ my_elf.h
@@ -0,0 +1,63 @@
+
+//{============================================================================
+//{                            MINIMAL ELF FILE                               ;
+//}============================================================================
+
+
+//-----------------------------------------------------------------------------
+// taken from:                                                                ;
+// https://code.woboq.org/linux/include/elf.h.html#65                         ;
+//-----------------------------------------------------------------------------
+
+#include <stdint.h>
+
+namespace elf
+{
+
+typedef uint16_t Elf64_Half;
+typedef uint32_t Elf64_Word;
+typedef uint64_t Elf64_Addr;
+typedef uint64_t Elf64_Off;
+typedef uint64_t Elf64_Xword;
+
+
+//{============================================================================
+//;                             ELF file header                               ;
+//}============================================================================
+
+struct Elf64_Ehdr
+{
+  unsigned char	e_ident[16];   	/* Magic number and other info       */
+  Elf64_Half	e_type;		    /* Object file type                  */
+  Elf64_Half	e_machine;	    /* Architecture                      */
+  Elf64_Word	e_version;	   	/* Object file version               */
+  Elf64_Addr	e_entry;    	/* Entry point virtual address       */
+  Elf64_Off	    e_phoff;    	/* Program header table file offset  */
+  Elf64_Off	    e_shoff;	    /* Section header table file offset  */
+  Elf64_Word	e_flags;	    /* Processor-specific flags          */
+  Elf64_Half	e_ehsize;		/* ELF header size in bytes          */
+  Elf64_Half	e_phentsize;    /* Program header table entry size   */
+  Elf64_Half	e_phnum;		/* Program header table entry count  */
+  Elf64_Half	e_shentsize;	/* Section header table entry size   */
+  Elf64_Half	e_shnum;		/* Section header table entry count  */
+  Elf64_Half	e_shstrndx;		/* Section header string table index */
+};
+
+
+//{============================================================================
+//;                          Program segment header                           ;
+//}============================================================================
+
+struct Elf64_Phdr
+{
+  Elf64_Word	p_type;	    	/* Segment type             */
+  Elf64_Word	p_flags;    	/* Segment flags            */
+  Elf64_Off 	p_offset;   	/* Segment file offset      */
+  Elf64_Addr	p_vaddr;    	/* Segment virtual address  */
+  Elf64_Addr	p_paddr;    	/* Segment physical address */
+  Elf64_Xword	p_filesz;   	/* Segment size in file     */
+  Elf64_Xword	p_memsz;	    /* Segment size in memory   */
+  Elf64_Xword	p_align;	    /* Segment alignment        */
+};
+
+}
