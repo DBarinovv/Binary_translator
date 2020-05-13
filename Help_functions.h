3aecefdc849c205d854894109f53f5d8f77f6662
DBarinovv <9653314755@bk.ru>
2020-05-13 03:21:56 +0300

    Big_test4

--- Help_functions.h
+++ Help_functions.h
@@ -11,15 +11,15 @@ int Check_If_Reg (char *elem)
     {
         return 0;
     }
-    else if (*elem == 'c' && *(elem + 1) == 'x')    // bx
+    else if (*elem == 'c' && *(elem + 1) == 'x')    // cx
     {
         return 1;
     }
-    else if (*elem == 'd' && *(elem + 1) == 'x')    // cx
+    else if (*elem == 'd' && *(elem + 1) == 'x')    // dx
     {
         return 2;
     }
-    else if (*elem == 'b' && *(elem + 1) == 'x')    // dx
+    else if (*elem == 'b' && *(elem + 1) == 'x')    // bx
     {
         return 3;
     }
